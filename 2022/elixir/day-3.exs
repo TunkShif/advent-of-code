56b8eeeb427d0b65aedddbd18b14fbd2747c100c
TunkShif <tunkshif@foxmail.com>
2022-12-08 00:07:42 +0800

    update: day 3 part 2 due tomorrow

---
+++ 2022/elixir/day-3.exs
@@ -0,0 +1,41 @@
+defmodule Day3 do
+  @input File.read!("./input-3.txt")
+  @sample ~S"""
+  vJrwpWtwJgWrhcsFMMfFFhFp
+  jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+  PmmdzqPrVvPwwTWBwg
+  wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+  ttgJtRGJQctTZtZT
+  CrZsJsPPZsGzwwsLwLmpwMDw
+  """
+
+  def test() do
+    157 = sum_of_items(@sample)
+  end
+
+  def solve() do
+    sum_of_items(@input) |> IO.puts()
+  end
+
+  defp priority(item) when item in ?a..?z, do: item - 96
+  defp priority(item) when item in ?A..?Z, do: item - 38
+
+  defp rucksacks_from(input) do
+    input
+    |> String.trim_trailing()
+    |> String.split("\n")
+    |> Enum.map(&{String.to_charlist(&1), String.length(&1)})
+    |> Enum.map(fn {chars, len} -> {Enum.map(chars, &priority/1), len} end)
+  end
+
+  defp sum_of_items(input) do
+    for {items, len} <- rucksacks_from(input) do
+      {first, last} = Enum.split(items, div(len, 2))
+      MapSet.intersection(MapSet.new(first), MapSet.new(last)) |> MapSet.to_list() |> List.first()
+    end
+    |> Enum.sum()
+  end
+end
+
+Day3.test()
+Day3.solve()
