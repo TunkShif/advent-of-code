129feaa980083f8578ea36f9d3c8c4d9383766d6
TunkShif <tunkshif@foxmail.com>
2023-12-10 14:14:04 +0800

    update: aoc day 1 par 1

--- 2023/ocaml/bin/solution_02.ml
+++ 2023/ocaml/bin/solution_02.ml
@@ -1,38 +1,47 @@
-let is_digit = function '0' .. '9' -> true | _ -> false
-let to_integer char = Char.code char - Char.code '0'
-
 module Game = struct
-  type t = { id : int; red : int; green : int; blue : int }
-  [@@deriving show, make, eq]
+  module Round = struct
+    type t = { red : int; green : int; blue : int } [@@deriving show]
 
-  let parse source =
-    let rec parse_aux game = function
-      | 'G' :: 'a' :: 'm' :: 'e' :: ' ' :: ch :: rest when is_digit ch ->
-          parse_aux { game with id = to_integer ch } rest
-      | ch :: ' ' :: 'r' :: 'e' :: 'd' :: rest when is_digit ch ->
-          parse_aux { game with red = game.red + to_integer ch } rest
-      | ch :: ' ' :: 'g' :: 'r' :: 'e' :: 'e' :: 'n' :: rest when is_digit ch ->
-          parse_aux { game with green = game.green + to_integer ch } rest
-      | ch :: ' ' :: 'b' :: 'l' :: 'u' :: 'e' :: rest ->
-          parse_aux { game with blue = game.blue + to_integer ch } rest
-      | _ :: rest -> parse_aux game rest
-      | [] -> game
-    in
-    parse_aux
-      (make ~id:0 ~red:0 ~green:0 ~blue:0)
-      (source |> String.to_seq |> List.of_seq)
+    let empty = { red = 0; green = 0; blue = 0 }
 
-  let id t = t.id
+    let can_play round =
+      round.red <= 12 && round.green <= 13 && round.blue <= 14
+  end
 
-  let can_play_with game ~red ~green ~blue =
-    game.red <= red && game.green <= green && game.blue <= blue
+  type t = { id : int; rounds : Round.t list } [@@deriving show]
 
-  let%test "game" =
-    Alcotest.(check bool)
-      "parse game" true
-      (equal
-         { id = 1; red = 5; green = 4; blue = 9 }
-         (parse "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"))
+  module Parser = struct
+    open Angstrom
+
+    let is_digit = function '0' .. '9' -> true | _ -> false
+    let integer = take_while1 is_digit >>| int_of_string
+    let red = integer <* string " red" >>| fun i -> `Red i
+    let green = integer <* string " green" >>| fun i -> `Green i
+    let blue = integer <* string " blue" >>| fun i -> `Blue i
+    let card = red <|> green <|> blue
+
+    let round =
+      sep_by1 (string ", ") card
+      >>| List.fold_left
+            Round.(
+              fun round card ->
+                match card with
+                | `Red i -> { round with red = round.red + i }
+                | `Green i -> { round with green = round.green + i }
+                | `Blue i -> { round with blue = round.blue + i })
+            Round.empty
+
+    let rounds = sep_by1 (string "; ") round
+
+    let game =
+      string "Game " *> integer <* string ": " >>= fun id ->
+      rounds >>| fun rounds -> { id; rounds }
+
+    let parse input = parse_string ~consume:All game input |> Result.get_ok
+  end
+
+  let parse = Parser.parse
+  let can_play game = List.for_all Round.can_play game.rounds
 end
 
 module Part_1 = struct
@@ -46,15 +55,12 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
     |}
     |> String.trim
 
-  let can_play = Game.can_play_with ~red:12 ~green:13 ~blue:14
-
   let solve input =
     input
     |> String.split_on_char '\n'
     |> List.map Game.parse
-    |> List.filter can_play
-    |> List.map Game.id
-    |> List.fold_left ( + ) 0
+    |> List.filter Game.can_play
+    |> List.fold_left Game.(fun acc game -> acc + game.id) 0
 
   let%test "part 1" = Alcotest.(check int) "part 1 sample" 8 (solve sample)
 end
